e4379634c6c783b28f8e103ecde51ff3a608ad53
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-04-15 09:56:14 -0500

    feat: generating roads

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { Point } from './primitives/point'
 import { Segment } from './primitives/segment'
 import { GraphEditor } from './graph-Editor'
 import { Viewport } from './viewport'
+import { Polygon } from './primitives/polygon'
+import { Envelope } from './primitives/envelope'
+import { World } from './world'
 
 
 
@@ -16,6 +19,7 @@ const ctx = myCanvas.getContext("2d")
 const graphString = localStorage.getItem("graph")
 const graphInfo = graphString ? JSON.parse(graphString) : null
 const graph = graphInfo ? Graph.load(graphInfo) : new Graph()
+const world = new World(graph)
 const viewport = new Viewport(myCanvas, graph)
 const graphEditor = new GraphEditor(viewport, graph)
 
@@ -23,6 +27,9 @@ animate()
 
 function animate() {
   viewport.reset()
+  world.generate()
+  world.draw(ctx)
+  ctx.globalAlpha = 0.03
   graphEditor.display()
   requestAnimationFrame(animate)
 }
--- src/math/utils.js
+++ src/math/utils.js
@@ -28,3 +28,43 @@ export const subtract = (point1, point2) => {
 export const scale = (point, factor) => {
   return new Point(point.x * factor, point.y * factor)
 }
+
+export const translate = (location, angle, offset) => {
+  return new Point(location.x + offset * Math.cos(angle), location.y + offset * Math.sin(angle))
+}
+
+export const angle = (p) => {
+  return Math.atan2(p.y, p.x)
+}
+
+export const getIntersection = (A,B,C,D ) => {
+  const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x)
+  const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y)
+  const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y)
+
+  if (bottom != 0) {
+    const t = tTop / bottom
+    const u = uTop / bottom
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t,
+      }
+    }
+  }
+  return null
+}
+
+const lerp = (a, b, t) => {
+  return a + (b - a) * t
+}
+
+export const getRanomColor = () => {
+  const hue = 290 + Math.random() * 260;
+  return "hsl(" + hue + ", 100%, 50%)"
+}
+
+export const average = (p1, p2) => {
+  return new Point( (p1.x + p2.x)/2 , (p1.y + p2.y)/2 )
+}
